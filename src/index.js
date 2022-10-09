dd90f0321fe49e7193029339839f242972a975c9
Jeison Ferrer <jeison120120@hotmail.com>
2022-10-08 22:43:31 -0500

    Un poquito arreglado

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const empleadoRouters = require('./routers/empleadoRoute');
 
 const app = express();
 
-
+//VARIABLES DE ENTORNO.
 const port = process.env.PORT || 9000; //Puerto por variable env.
 const URI = process.env.MONGODB_URI; //Conexión con la db
+
+
 
 app.use(express.json());//Para usar JSON en las respuestas y peticiones.
 app.use(express.urlencoded({extended: false}));
 
+
+
 /**
- * 
+ * RUTAS
  */
-const empleadoRouters = require('./routers/empleadoRoute');
-
-
 app.use('/api/empleados',empleadoRouters);
 
 
@@ -27,7 +29,6 @@ app.listen(port, ()=> console.log('server listening on port '+port)); // Servido
 /**
  * Conexión con la base de datos MongoDB atlas.
  */
-//mongoose.set('useCreateIndex',true);
 const conectar = ()=>{
     mongoose.connect(URI).then(()=>{
         console.log('Conected with mongodb atlas')
@@ -36,13 +37,4 @@ const conectar = ()=>{
 
 conectar(); //Iniciar conexión con la DB
 
-/**
- * const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://jkbnomina-user:<password>@jkbnomina.gfn5ls2.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
- */
+
